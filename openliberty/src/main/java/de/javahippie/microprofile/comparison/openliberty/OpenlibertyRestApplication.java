6e3d290eec8d5e9d1d5b57bd1df327c7003b05e1
Tim Zöller <tim.zoeller@ilume.de>
2019-10-26 07:57:12 +0200

    Added openliberty

---
+++ openliberty/src/main/java/de/javahippie/microprofile/comparison/openliberty/OpenlibertyRestApplication.java
@@ -0,0 +1,11 @@
+package de.javahippie.microprofile.comparison.openliberty;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class OpenlibertyRestApplication extends Application {
+}
