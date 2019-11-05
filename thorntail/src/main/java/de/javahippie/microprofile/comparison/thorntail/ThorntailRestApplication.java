e3024830e48dbd8dc30e701a11ee4f41b025b0a4
Tim Zöller <tim.zoeller@ilume.de>
2019-11-05 22:59:16 +0100

    Added thorntail

---
+++ thorntail/src/main/java/de/javahippie/microprofile/comparison/thorntail/ThorntailRestApplication.java
@@ -0,0 +1,11 @@
+package de.javahippie.microprofile.comparison.thorntail;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class ThorntailRestApplication extends Application {
+}
